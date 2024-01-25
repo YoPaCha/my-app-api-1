0b993de38ee91b5e4a0b128f1da606652fc4274a
Yoann Chaplain <yoannchaplain78@gmail.com>
2024-01-25 15:06:22 +0100

    small fixes

--- src/test/getArticles.test.js
+++ src/test/getArticles.test.js
@@ -1,45 +1,30 @@
-const { getArticles } = require("../controllers/articleController");
+const { getArticles } = require("../controllers/article.controller");
 const db = require("../models/");
 const { ARTICLES } = require("./data/articles");
-// const faker = require("faker");
 
 jest.mock('../models/', () => (
     {
         Article: {
             findAll: jest.fn()
-        }
+        },
     }
 ));
 
 describe("Controller getArticles", () => {
-    it("It should return a res 200 with a list of articles as an array of objects", async () => { 
-        // Utiliser Faker pour générer des données aléatoires
-        // const numberOfArticles = 5;
-
+    it("It should return a res 200 with a list of articles as an array of objects", async () => {
+        //utiliser faker pour générer des données aléatoires
         const mockup = ARTICLES;
-        
-        /*
-        // Mock the function to return the generated articles
-        jest.spyOn(Article, 'find').mockResolvedValueOnce(mockup);
-
-        // Call the function and test the response
-        const response = await getArticles();
-
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual(mockup);
-        */
-        
-        // On simule la résolution de la promesse de la méthode findAll()
-        db.Article.findAll.mockResolvedValueOnce(mockup.results);
-        // On simule les méthodes json() et status() de l'objet res
+        // on simule la résolution de la promesse de la méthode findAll() en passant le jeu de données à tester
+        db.Article.findAll.mockResolvedValue(mockup);
+        // on simule les méthodes json() et status() de l'objet res
         const mockJson = jest.fn();
         const mockStatus = jest.fn(() => ({ json: mockJson }));
-        // On simule l'objet res
+        // on simule l'objet res
         const res = { status: mockStatus };
-        // On simule l'objet req
+        // on simule l'objet req
         const req = {};
 
-        // On appelle la méthode gotArticles
+        // on appelle la méthode getArticles
         await getArticles(req, res);
 
         // on vérifie que la méthode json() renvoie bien le mockup
@@ -48,16 +33,11 @@ describe("Controller getArticles", () => {
             success: true
         });
 
+        // expect 200 status
         expect(mockStatus).toHaveBeenCalledWith(200);
 
     });
-
-    it("It should return a res 404 not found with an empty list of articles", async () => {     
-        /* const mockup = Array.from( () => ({
-            message: "Not found",
-            success: false
-        })); */
-        
+    it("It should return a res 404 not found with an empty array of articles", async () => {
         const mockup = [];
         db.Article.findAll.mockResolvedValue(mockup);
         const mockJson = jest.fn();
@@ -76,8 +56,7 @@ describe("Controller getArticles", () => {
             message: "No articles found"
         });
     });
-
-    it("It should return a res 500 error", async () => { 
+    it("It should return a res 500 error", async () => {
         const mockup = new Error("Internal server error");
         db.Article.findAll.mockRejectedValue(mockup);
         const mockJson = jest.fn();
